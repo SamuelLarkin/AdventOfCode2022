b46b813c0bef82eaf854e1259f73fe172963c274
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2022-12-05 07:27:15 -0500

    A solution template for a quicker start.

---
+++ solution.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env  python3
+
+from typing import (
+        Generator,
+        List,
+        Tuple,
+        )
+
+
+
+def parser() -> Generator[, None, None]:
+    """
+    """
+    with open("data", mode="r", encoding="UTF8") as fin:
+        for line in map(str.strip, fin):
+            pass
+
+
+
+def part1() -> int:
+    """
+    """
+    return 0
+
+
+
+def part2() -> int:
+    """
+    """
+    return 0
+
+
+
+
+
+if __name__ == "__main__":
+    answer = part1()
+    print(f"Part1 answer: {answer}")
+    assert answer == 69289
+
+    answer = part2()
+    print(f"Part2 answer: {answer}")
+    assert answer == 205615
