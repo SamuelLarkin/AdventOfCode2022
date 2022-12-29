5742111dedfd3b1a84a8605d6075e80fb46b9488
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2022-12-29 14:18:48 -0500

    Show the wrong answer on assertion failure.

--- solution.py
+++ solution.py
@@ -42,9 +42,9 @@ if __name__ == "__main__":
     assert (answer := part1("test")) == 152, answer
     answer = part1()
     print(f"Part1 answer: {answer}")
-    assert answer == 69289
+    assert answer == 69289, answer
 
     assert (answer := part2("test")) == 301, answer
     answer = part2()
     print(f"Part2 answer: {answer}")
-    assert answer == 205615
+    assert answer == 205615, answer
