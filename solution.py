3e6f761cd899688c877b73d30568b64808858da8
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2022-12-29 14:18:31 -0500

    Assert part1 and part2 works with test.

--- solution.py
+++ solution.py
@@ -39,10 +39,12 @@ def part2(data: str="data") -> int:
 
 
 if __name__ == "__main__":
+    assert (answer := part1("test")) == 152, answer
     answer = part1()
     print(f"Part1 answer: {answer}")
     assert answer == 69289
 
+    assert (answer := part2("test")) == 301, answer
     answer = part2()
     print(f"Part2 answer: {answer}")
     assert answer == 205615
